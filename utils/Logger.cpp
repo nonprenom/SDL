4bbd629bab8b06cff29cda8365c7a17725e677db
Erwan Eitan <erwanei@essence-grp.com>
2024-06-21 21:40:13 +0300

    better logger

--- utils/Logger.cpp
+++ utils/Logger.cpp
@@ -1,25 +1,102 @@
 #include <cstdarg>
 #include <cstdio>
 #include <ctime>
+#include <windows.h>
+#include <string>
+#include <stdio.h>
 
 #include "Logger.h"
 
+Logger &Logger::getInstance()
+{
+	static Logger instance;
+	return instance;
+}
+
+Logger::~Logger()
+{
+	LOG_DBG("Closing Logger");
+	if (m_outputs & OUTPUT_TO_FILE)
+	{
+		m_file.close();
+	}
+}
+
+void Logger::initialize()
+{
+	// build the INI filename (process_name.ini)
+	char INIfileName[MAX_PATH + 1];
+	GetModuleFileName(NULL, INIfileName, sizeof(INIfileName));
+	strncat(INIfileName, ".ini", sizeof(INIfileName) - strlen(INIfileName));
+
+	// build the default log file name (process_name.log)
+	char LOGfileName[MAX_PATH + 1];
+	GetModuleFileName(NULL, LOGfileName, sizeof(LOGfileName));
+	strncat(LOGfileName, ".log", sizeof(LOGfileName) - strlen(LOGfileName));
+
+	m_outputs = INITIALIZED;
+
+	// Read the config from file
+	char buf[8];
+
+	GetPrivateProfileString("Logger", "Console", "true", buf, 5, INIfileName);
+	m_outputs |= (std::string(_strlwr(buf)) == "true") * OUTPUT_TO_CONSOLE;
+
+	GetPrivateProfileString("Logger", "File", "false", buf, 5, INIfileName);
+	m_outputs |= (std::string(_strlwr(buf)) == "true") * OUTPUT_TO_FILE;
+
+	GetPrivateProfileString("Logger", "Filename", LOGfileName, LOGfileName, sizeof(LOGfileName), INIfileName);
+
+	if (m_outputs & OUTPUT_TO_FILE)
+	{
+		m_file.open(LOGfileName, std::ios_base::app);
+		if (m_file.is_open() == false)
+		{
+			m_outputs = OUTPUT_TO_CONSOLE;
+			LOG_ERROR("Unable to open the log file %s : ", strerror(errno));
+		}
+	}
+
+	LOG_INFO("Read configuration:");
+	LOG_INFO("Console : %s", (m_outputs & OUTPUT_TO_CONSOLE) ? "yes" : "no");
+	LOG_INFO("File : %s", (m_outputs & OUTPUT_TO_FILE) ? "yes" : "no");
+	LOG_INFO("Filename : %s", LOGfileName);
+}
+
 void Logger::log(const char *logType, const char *sourceFile, const int sourceLine, const char *fmt, ...)
 {
+	if (m_outputs == NOT_INITIALIZED)
+	{
+		initialize();
+	}
+
+	if (m_outputs == INITIALIZED /* only */)
+	{
+		return;
+	}
+
 	struct timespec ts;
 	clock_gettime(CLOCK_REALTIME, &ts);
 	struct tm *timeinfo = localtime(&ts.tv_sec);
 
-	char buff[100];
+#define MAXLOGLINESIZE 1024
 
-	strftime(buff, sizeof(buff), "%d/%m/%y %H:%M:%S", timeinfo);
-	fprintf(stdout, "%s.%03ld [%-6s] (%s:%d)", buff, ts.tv_nsec / 1000000, logType, sourceFile, sourceLine);
+	char output[MAXLOGLINESIZE];
+	strftime(output, sizeof(output), "%d/%m/%y %H:%M:%S", timeinfo);
+	snprintf(output + strlen(output), MAXLOGLINESIZE - strlen(output), ".%03ld [%-6s] (%s:%d) ", ts.tv_nsec / 1000000, logType, sourceFile, sourceLine);
 
 	va_list args;
 	va_start(args, fmt);
-	char buffer[1024];
-	vsnprintf(buffer, 1024, fmt, args);
-	fprintf(stdout, buffer);
+	vsnprintf(output + strlen(output), MAXLOGLINESIZE - strlen(output), fmt, args);
 	va_end(args);
-	fprintf(stdout, "\r\n");
+	sprintf(output + strlen(output), "\n");
+
+	if (m_outputs & OUTPUT_TO_CONSOLE)
+	{
+		printf(output);
+	}
+	if (m_outputs & OUTPUT_TO_FILE)
+	{
+		m_file.write(output, strlen(output));
+	}
 }
--- utils/Logger.h
+++ utils/Logger.h
@@ -1,12 +1,34 @@
+#include <stdint.h>
+#include <windows.h>
+#include <fstream>
+#include <iostream>
 class Logger
 {
+public:
+	Logger(Logger const &) = delete;
+	void operator=(Logger const &) = delete;
+	static Logger &getInstance();
+
+	enum
+	{
+		NOT_INITIALIZED = 0,
+		INITIALIZED = 1,
+		OUTPUT_TO_CONSOLE = 2,
+		OUTPUT_TO_FILE = 4
+	};
+
+	void log(const char *logType, const char *sourceFile, const int sourceLine, const char *fmt, ...);
+
 private:
 	Logger() = default;
-	~Logger() = default;
+	~Logger();
 
-public:
-	static void log(const char *logType, const char *sourceFile, const int sourceLine, const char *fmt, ...);
+	uint8_t m_outputs = NOT_INITIALIZED;
+	std::ofstream m_file;
+
+	void initialize();
 };
 
-#define LOG_INFO(...) Logger::log("INFO", __FILE__, __LINE__, __VA_ARGS__)
-#define LOG_DBG(...) Logger::log("DEBUG", __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_INFO(...) Logger::getInstance().log("INFO", __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_DBG(...) Logger::getInstance().log("DEBUG", __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_ERROR(...) Logger::getInstance().log("ERROR", __FILE__, __LINE__, __VA_ARGS__)
